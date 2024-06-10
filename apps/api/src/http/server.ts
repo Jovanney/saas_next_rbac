d71d3a1f9271520419dc80235785a0f676601d67
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-06-09 21:44:02 -0300

    feat: Add rejectInvite and revokeInvite routes, and getPendingInvites and getOrganizationBilling routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -35,6 +35,10 @@ import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
 import { acceptInvite } from './routes/invites/accept-invite'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { getOrganizationBilling } from './routes/billing/get-organization-billing'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -111,6 +115,13 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
+app.register(getPendingInvites)
+
+//BIlling
+
+app.register(getOrganizationBilling)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')

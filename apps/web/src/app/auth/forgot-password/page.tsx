2d783aa9a2c4a1e78074ee46700f497bf2a8865c
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-06-10 08:15:45 -0300

    feat: sign-up and forgot password pages

--- apps/web/src/app/auth/forgot-password/page.tsx
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-export default function SignInPage() {
+export default function ForgotPasswordPage() {
   return (
     <form action="" className="space-y-4">
       <div className="space-y-1">
@@ -12,24 +12,12 @@ export default function SignInPage() {
         <Input name="email" type="email" id="email" />
       </div>
 
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
-      </div>
-
       <Button className="w-full" type="submit">
-        Sign in with e-mail
+        Recover password
       </Button>
 
       <Button className="w-full" variant="link" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Sign in instead</Link>
       </Button>
     </form>
   )

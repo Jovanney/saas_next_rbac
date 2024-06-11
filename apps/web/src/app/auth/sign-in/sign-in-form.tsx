d129db49d3f93081ee8feafd06f3cad86be5e9e7
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-06-11 09:19:23 -0300

    feat: Refactor sign-in form to use useFormState hook

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -10,39 +10,15 @@ import { signInWithEmailAndPassword } from './actions'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useFormState } from '@/hooks/use-form-state'
 
 export function SignInForm() {
-  // const [{ errors, message, success }, formAction, isPending] = useActionState(
-  //   signInWithEmailAndPassword,
-  //   { success: false, message: null, errors: null },
-  // )
-
-  const [isPending, startTransition] = useTransition()
-  const [{ success, message, errors }, setFormState] = useState<{
-    success: boolean
-    message: string | null
-    errors: Record<string, string[]> | null
-  }>({
-    success: false,
-    message: null,
-    errors: null,
-  })
-
-  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const form = event.currentTarget
-    const data = new FormData(form)
-
-    startTransition(async () => {
-      const state = await signInWithEmailAndPassword(data)
-
-      setFormState(state)
-    })
-  }
+  const [{ errors, message, success }, handleSubmit, isPending] = useFormState(
+    signInWithEmailAndPassword
+  )
 
   return (
-    <form onSubmit={handleSignIn} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
       {success === false && message && (
         <Alert variant="destructive">
           <AlertTriangle className="size-4" />

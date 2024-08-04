f8619f86d7c8956bfc8a5b1f85fbeb755adda61d
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-08-04 12:32:43 -0300

    feat: Add dayjs library for date manipulation and formatting
    This commit adds the dayjs library as a new dependency to the project. Dayjs is a lightweight JavaScript library for date manipulation and formatting, providing a simpler and more intuitive API compared to other date libraries. This addition will enable the use of relative time formatting in the project's project list component, improving the user experience by displaying more human-readable timestamps.

--- apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
@@ -1,5 +1,7 @@
 import { ArrowRight } from 'lucide-react'
-
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { getProjects } from '@/http/get-projects'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,37 +11,51 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { getCurrentOrg } from '@/app/auth/auth'
+
+dayjs.extend(relativeTime)
 
 export async function ProjectList() {
+  const currentOrg = getCurrentOrg()
+  const { projects } = await getProjects(currentOrg!)
   return (
     <div className="grid grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Projeto 01</CardTitle>
-          <CardDescription className="line-clamp-2 leading-relaxed">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
-            iusto unde nisi eaque delectus dignissimos nemo fuga, sint numquam
-            aspernatur sed magni harum amet nostrum ducimus doloribus totam
-            deleniti placeat?
-          </CardDescription>
-        </CardHeader>
-        <CardFooter className="flex items-center gap-1.5">
-          <Avatar className="size-4">
-            <AvatarImage src="https://github.com/diego3g.png" />
-            <AvatarFallback />
-          </Avatar>
+      {projects.map((project) => {
+        return (
+          <Card
+            key={project.id}
+            className="flex min-w-fit flex-col justify-between"
+          >
+            <CardHeader>
+              <CardTitle className="text-xl font-medium">
+                {project.name}
+              </CardTitle>
+              <CardDescription className="line-clamp-2 leading-relaxed">
+                {project.description}
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="flex items-center gap-1.5">
+              <Avatar className="size-4">
+                {project.owner.avatarUrl && (
+                  <AvatarImage src={project.owner.avatarUrl} />
+                )}
+                <AvatarFallback />
+              </Avatar>
 
-          <span className="text-xs text-muted-foreground">
-            Created by{' '}
-            <span className="font-medium text-foreground">Diego Fernandes</span>{' '}
-            a day ago
-          </span>
+              <span className="truncate text-xs text-muted-foreground">
+                <span className="font-medium text-foreground">
+                  {project.owner.name}
+                </span>{' '}
+                {dayjs(project.createdAt).fromNow()}
+              </span>
 
-          <Button size="xs" variant="outline" className="ml-auto">
-            View <ArrowRight className="ml-2 size-3" />
-          </Button>
-        </CardFooter>
-      </Card>
+              <Button size="xs" variant="outline" className="ml-auto">
+                View <ArrowRight className="ml-2 size-3" />
+              </Button>
+            </CardFooter>
+          </Card>
+        )
+      })}
     </div>
   )
 }
